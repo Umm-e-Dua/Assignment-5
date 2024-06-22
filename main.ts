0a39cb893a4c2fed1c1a98d63b8ce4023474d68d
Umm-e-Dua <160483094+Umm-e-Dua@users.noreply.github.com>
2024-06-23 01:25:56 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,4 @@
+// 5th assignment
+let famousperson = "Albert Einstein"
+let message = ("once said, “A person who never made a mistake never tried anything new.”");
+console.log(famousperson , message);
